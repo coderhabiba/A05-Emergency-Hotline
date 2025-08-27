a6b70ee122bff3100634ce72f6d0241fc74fd3fb
coderhabiba <anikahabiba50@gmail.com>
2025-08-27 18:47:59 +0600

    call history area functionality done

--- main.js
+++ main.js
@@ -30,9 +30,42 @@ for (let btn of heartBtns) {
   });
 }
 
+
 // call btns click function
+let coinDiv = document.getElementById('nav-coin-count');
+let coins = parseInt(coinDiv.innerText);
 
+const d = new Date();
+const time = d.toLocaleTimeString();
 
+let historyArea = document.getElementById('right-area');
+
+const callBtns = document.querySelectorAll('.call-btn');
+
+for (let btn of callBtns){
+  btn.addEventListener('click', function(e) {
+    alert(
+      `📞 Calling ${
+        e.target.parentNode.parentNode.querySelector('h6').innerText
+      } ${e.target.parentNode.parentNode.querySelector('p').innerText}...`
+    );
+    coins -= 20;
+    coinDiv.innerText = coins;
+    
+    historyArea.innerHTML += `
+  
+  <div>
+    <div class="flex items-center justify-between">
+      <div class="flex flex-col gap-1">
+        <p>${e.target.closest('.card').querySelector('h6').innerText}</p>
+        <p>${e.target.closest('.card').querySelector('p').innerText}</p>
+      </div>
+      <div>${time}</div>
+    </div>
+  </div>
+`;
+  })
+}
 
 
 
